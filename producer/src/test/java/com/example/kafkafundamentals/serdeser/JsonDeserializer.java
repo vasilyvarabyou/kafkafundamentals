dc28067437efe2b051e7b270404c8a7d2db32734
gl_vvarabyou <vasil.varabyou@godel.shellenergy.co.uk>
2024-06-25 14:55:39 +0200

    Consumer

---
+++ producer/src/test/java/com/example/kafkafundamentals/serdeser/JsonDeserializer.java
@@ -0,0 +1,32 @@
+package com.example.kafkafundamentals.consumer.serdeser;
+
+import java.io.IOException;
+import java.util.Map;
+
+import org.apache.kafka.common.errors.SerializationException;
+import org.apache.kafka.common.serialization.Deserializer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JsonDeserializer<T> implements Deserializer<T> {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    private Class<T> clazz;
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public void configure(final Map<String, ?> configs, final boolean isKey) {
+        clazz = (Class<T>) configs.get("JsonClass");
+    }
+
+    @Override
+    public T deserialize(final String topic, final byte[] data) {
+        if (data == null) {
+            return null;
+        }
+        try {
+            return objectMapper.readValue(data, clazz);
+        } catch (IOException e) {
+            throw new SerializationException(e);
+        }
+    }
+}
